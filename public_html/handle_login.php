ef261967f8f30410b27b0ef8948514523f0fa86a
Cole Holland <cole.holland@my.simpson.edu>
2016-12-12 11:04:46 -0600

    Login Handling.

---
+++ public_html/handle_login.php
@@ -0,0 +1,24 @@
+$c = curl_init('https://api.amazon.com/auth/o2/tokeninfo?access_token=' . urlencode($_REQUEST['access_token']));
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+ 
+$r = curl_exec($c);
+curl_close($c);
+$d = json_decode($r);
+ 
+if ($d->aud != 'amzn1.application-oa2-client.be56c2f0c5e849a5b3c1f8568af7342f 
+') {
+  header('HTTP/1.1 404 Not Found');
+  echo 'Page not found';
+  exit;
+}
+ 
+// exchange the access token for user profile
+$c = curl_init('https://api.amazon.com/user/profile');
+curl_setopt($c, CURLOPT_HTTPHEADER, array('Authorization: bearer ' . $_REQUEST['access_token']));
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+ 
+$r = curl_exec($c);
+curl_close($c);
+$d = json_decode($r);
+ 
+echo sprintf('%s %s %s', $d->name, $d->email, $d->user_id);
